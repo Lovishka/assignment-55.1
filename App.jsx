4900d612c15811024e54f4280e2973bb3f12b6c2
Lovishka <lovishkasakuja1805@gmail.com>
2025-07-30 16:54:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Table from './table';
+
+function App() {
+  
+  return (
+    <>
+    <div className='flex m-2 gap-2'>
+      <Table></Table>
+      <Table></Table>
+    </div>
+      
+    </>
+  );
+}
+
+export default App
